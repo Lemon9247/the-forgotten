be5511efa2660deca1fbbbb4d352023ad6faebbe
Willow <willow.sparks002@gmail.com>
2025-02-25 18:39:19 +0000

    Update mod details and add client

---
+++ src/main/java/com/lemoneater/theforgotten/TheForgottenClient.java
@@ -0,0 +1,10 @@
+package com.lemoneater.theforgotten;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheForgottenClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
