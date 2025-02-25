6493c526105a4a5d496b0e4ad2dcff3313a2f127
Willow <willow.sparks002@gmail.com>
2025-02-25 17:53:06 +0000

    Initial commit

---
+++ src/client/java/com/lemoneater/theforgotten/TheForgottenDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lemoneater.theforgotten;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheForgottenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
